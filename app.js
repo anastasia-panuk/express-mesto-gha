fc9ae4d6822ccf7cb3e71b78723029c336ccfd42
Anastasia Panuk <AnaPanana@yandex.ru>
2022-11-17 14:21:20 +0300

    fix: исправление ошибок автотестирования

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
+const NOT_FOUND_ERR = 404;
+
 app.use(bodyParser.json());
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
@@ -20,6 +22,10 @@ app.use((req, res, next) => {
 
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
+
+app.use('*', (req, res) => {
+  res.status(NOT_FOUND_ERR).send({ message: 'Страница не найдена' });
+});
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
