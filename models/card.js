21d9ad319c20af227eda9fb767047dcfddfd18ac
Anastasia Panuk <AnaPanana@yandex.ru>
2022-11-20 19:54:44 +0300

    fix: удаление лишней зависимости и изменение типа данных в схеме карточки

--- models/card.js
+++ models/card.js
@@ -18,7 +18,7 @@ const cardSchema = new mongoose.Schema({
   },
   likes: [{
     type: mongoose.Schema.Types.ObjectId,
-    default: true,
+    default: [],
     ref: 'user',
   }],
   createdAt: {
